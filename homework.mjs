0185b9059723c6414106b1696905291140b99277
LiliyaLexx <liliya.fedyurina@gmail.com>
2024-05-07 14:55:37 -0700

    First problem solved

--- homework.mjs
+++ homework.mjs
@@ -1,1 +1,13 @@
-console.log("Hello")
+let i = 0;
+
+for( let i = 1; i < 100; i++){
+    if(i % 3=== 0){
+        console.log("Fizz");
+    } else if ( i % 5 === 0){
+        console.log("Buzz");
+    } else if ( i % 3 === 0 && i % 5 ===0 ){
+        console.log( "Finn Buzz");
+    } else {
+        console.log(i);
+    }
+}
