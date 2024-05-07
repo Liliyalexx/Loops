be34b2a1d50c539076dffb3d888f96f601cacfd3
LiliyaLexx <liliya.fedyurina@gmail.com>
2024-05-07 15:41:17 -0700

    Part 3: Feeling Loopy added

--- homework.mjs
+++ homework.mjs
@@ -29,7 +29,28 @@ for (let i = n + 1; i > 0; i++) {
     break;
 }
   
+//Part 3: Feeling Loopy
 
+const csv = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
+
+let cell = '';
+let row = [];
+
+for (let i = 0; i < csv.length; i++) {
+  let char = csv[i];
+
+  if (char === ',') {
+    row.push(cell);
+    cell = '';
+  } else if (char === '\n') {
+    row.push(cell);
+    console.log(row.join(', '));
+    row = [];
+    cell = '';
+  } else {
+    cell += char;
+  }
+}
 
 
 
